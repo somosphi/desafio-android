fdc648f95588e3802b539775b1bc3680f4436e9b
Newton <newtoljunior@hotmail.com>
2021-01-05 19:39:45 -0300

    atualizando a arquitetura

--- phi/app/src/main/java/com/newton/phi/view/activity/MainActivity.kt
+++ phi/app/src/main/java/com/newton/phi/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.newton.phi
+package com.newton.phi.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.newton.phi.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
