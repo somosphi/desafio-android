b250f7abc22c549a14896f95be8a510adf5ecb4e
Pedro Guimarães <moisespedro15@gmail.com>
2021-01-06 15:50:16 -0300

    Add fragment for statement detail

---
+++ app/src/main/java/com/example/desafiophi/features/bankStatementDetail/BankStatementDetailFragment.kt
@@ -0,0 +1,12 @@
+package com.example.desafiophi.features.bankStatementDetail
+
+import androidx.fragment.app.Fragment
+import com.example.desafiophi.R
+import com.example.desafiophi.architecture.android.viewBinding
+import com.example.desafiophi.databinding.FragmentBankStatementDetailBinding
+
+class BankStatementDetailFragment : Fragment(R.layout.fragment_bank_statement_detail) {
+
+    private val binding by viewBinding(FragmentBankStatementDetailBinding::bind)
+
+}
