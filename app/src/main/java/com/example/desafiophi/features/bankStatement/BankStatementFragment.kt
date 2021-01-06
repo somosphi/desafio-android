49962a09779a059e2d5ea2bf6334fcf049160aa8
Pedro Guimarães <moisespedro15@gmail.com>
2021-01-06 10:24:03 -0300

    Initialize navigation

---
+++ app/src/main/java/com/example/desafiophi/features/bankStatement/BankStatementFragment.kt
@@ -0,0 +1,6 @@
+package com.example.desafiophi.features.bankStatement
+
+import androidx.fragment.app.Fragment
+import com.example.desafiophi.R
+
+class BankStatementFragment : Fragment(R.layout.fragment_bank_statement)
