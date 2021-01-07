e27a9da0e31023c9a9cea4233f31e2e8d3cd3ae8
Newton <newtoljunior@hotmail.com>
2021-01-07 19:49:41 -0300

    alteracao no tipo das variaveis do model

--- phi/app/src/main/java/com/newton/phi/model/Transaction.kt
+++ phi/app/src/main/java/com/newton/phi/model/Transaction.kt
@@ -1,9 +1,9 @@
 package com.newton.phi.model
 
 class Transaction (
-    val description : String,
-    val name : String,
-    val value : String,
-    val pix : Boolean,
-    val date : String,
+    var description : String,
+    var name : String,
+    var value : String,
+    var pix : Boolean,
+    var date : String,
 )
