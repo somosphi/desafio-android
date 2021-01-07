a77a73b11d45dc4dd801153cbca0a13370142887
Newton <newtoljunior@hotmail.com>
2021-01-07 19:35:51 -0300

    criacao do model

---
+++ phi/app/src/main/java/com/newton/phi/model/Transaction.kt
@@ -0,0 +1,9 @@
+package com.newton.phi.model
+
+class Transaction (
+    val description : String,
+    val name : String,
+    val value : String,
+    val pix : Boolean,
+    val date : String,
+)
