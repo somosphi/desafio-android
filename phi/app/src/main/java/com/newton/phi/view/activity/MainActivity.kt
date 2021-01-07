c4e05780243bd386f952ea030c147479c0a38b40
Newton <newtoljunior@hotmail.com>
2021-01-06 22:56:25 -0300

    implementacao da base da navegacao

--- phi/app/src/main/java/com/newton/phi/view/activity/MainActivity.kt
+++ phi/app/src/main/java/com/newton/phi/view/activity/MainActivity.kt
@@ -2,11 +2,17 @@ package com.newton.phi.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.newton.phi.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = findNavController(R.id.fragment)// where nav_host_fragment is the id for your Main NavHost fragment
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
