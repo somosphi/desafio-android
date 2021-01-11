e971f111bfbf9d80b926a0c310dfc240e6a13155
Newton <newtoljunior@hotmail.com>
2021-01-11 07:32:53 -0300

    modulos do koin

--- phi/app/src/main/java/com/newton/phi/di/modules.kt
+++ phi/app/src/main/java/com/newton/phi/di/modules.kt
@@ -1,2 +1,21 @@
 package com.newton.phi.di
 
+import com.newton.phi.network.Interector
+import com.newton.phi.network.RetrofitInicializer
+import com.newton.phi.view.viewmodel.TransactionViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val moduleViewModel = module {
+    viewModel { TransactionViewModel(get(),androidContext()) }
+}
+
+val moduleInicializerRequest = module {
+    factory { RetrofitInicializer() }
+}
+
+val moduleInterector = module {
+    factory { Interector(get()) }
+}
+
