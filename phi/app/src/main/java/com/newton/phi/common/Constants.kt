b8535bffd2e4770c3cb918c5acc37dc2b417d8ad
Newton <newtoljunior@hotmail.com>
2021-01-11 07:30:56 -0300

    constantes

---
+++ phi/app/src/main/java/com/newton/phi/common/Constants.kt
@@ -0,0 +1,6 @@
+package com.newton.phi.common
+
+object Constants {
+    val baseUrl = "https://desafio-mobile-bff.herokuapp.com/"
+    val baseToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+}
