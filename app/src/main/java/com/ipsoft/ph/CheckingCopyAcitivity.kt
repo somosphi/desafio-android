559cc9b81800f4b1f03528c3614aa4b2c34c9e99
AnthoniIP <anthoni.ipiranga@gmail.com>
2021-01-18 19:13:13 -0300

    Finalizado layout da activity_statement.xml

---
+++ app/src/main/java/com/ipsoft/ph/CheckingCopyAcitivity.kt
@@ -0,0 +1,11 @@
+package com.ipsoft.ph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckingCopyAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checking_copy_acitivity)
+    }
+}
---
+++ app/src/main/java/com/ipsoft/ph/StatementActivity.kt
@@ -0,0 +1,11 @@
+package com.ipsoft.ph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StatementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statement)
+    }
+}
