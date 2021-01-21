8408f1db9e9ba6d888a4a807a2f01d399cf7fa1b
AnthoniIP <anthoni.ipiranga@gmail.com>
2021-01-20 22:24:41 -0300

    Removidos livedatas,deixando somente as requisições dinamicas

--- app/src/main/java/com/ipsoft/ph/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ipsoft/ph/viewmodel/MainViewModel.kt
@@ -1,7 +1,6 @@
 package com.ipsoft.ph.viewmodel
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.ipsoft.ph.repository.HttpRepository
@@ -19,26 +18,24 @@ import com.ipsoft.ph.repository.model.TransactionResponse
 
 class MainViewModel(private val repository: HttpRepository) : ViewModel() {
 
-    var transactionsLiveData = MutableLiveData<TransactionResponse>()
-    var balanceLiveData = MutableLiveData<Balance>()
-    var detailsLiveData = MutableLiveData<Transaction>()
+
 
     fun getBalance(): LiveData<Balance> {
 
-        balanceLiveData = repository.getBalance()
-        return balanceLiveData
+
+        return repository.getBalance()
     }
 
     fun getTransactions(): LiveData<TransactionResponse> {
 
-        transactionsLiveData = repository.getTransactions()
-        return transactionsLiveData
+
+        return repository.getTransactions()
     }
 
     fun getDetailTransaction(id: String): LiveData<Transaction> {
 
-        detailsLiveData = repository.getDetailTransaction(id)
-        return detailsLiveData
+
+        return repository.getDetailTransaction(id)
 
     }
 
