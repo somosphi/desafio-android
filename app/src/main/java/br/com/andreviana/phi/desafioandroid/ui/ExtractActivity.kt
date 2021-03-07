36dfcd0acb38ae18b797c466f7290aac56036b45
Andre Viana <icaro-andre@hotmail.com>
2021-03-07 18:49:19 -0300

    Finalizado desenho de tela do extrato.

---
+++ app/src/main/java/br/com/andreviana/phi/desafioandroid/ui/ExtractActivity.kt
@@ -0,0 +1,12 @@
+package br.com.andreviana.phi.desafioandroid.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.andreviana.phi.desafioandroid.R
+
+class ExtractActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_extract)
+    }
+}
