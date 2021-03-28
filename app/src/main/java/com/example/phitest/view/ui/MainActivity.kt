8a75ee09f53eb0bf5100956fb158ad048136fffe
vieiraclaudio <claudio_vieira@ymail.com>
2021-03-28 13:51:30 -0300

    commit inicial

---
+++ app/src/main/java/com/example/phitest/view/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.phitest.view.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.phitest.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
