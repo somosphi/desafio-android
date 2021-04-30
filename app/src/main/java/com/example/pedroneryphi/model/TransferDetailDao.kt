6293e7a6a09b49ca9681a568022de57d57bf7db5
Pedro <pedronery2608@gmail.com>
2021-04-30 10:34:42 -0300

    Adjust room

--- app/src/main/java/com/example/pedroneryphi/model/TransferDetailDao.kt
+++ app/src/main/java/com/example/pedroneryphi/model/TransferDetailDao.kt
@@ -1,9 +1,11 @@
 package com.example.pedroneryphi.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "transfers")
 data class TransferDetailDao(
+    @PrimaryKey
     var id: String,
     var amount: Int,
     var to: String,
